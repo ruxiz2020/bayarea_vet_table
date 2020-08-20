73b991deafe500f5a3381981d5343514f2a362f8
zruxi <zruxi@vmware.com>
2020-08-19 21:49:50 -0700

    screen capture

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ breed_list = ['Cat', 'Dog', 'Bird', 'Rodent', 'Rabbit', 'Snake', "Other"]
 df_review = pd.read_csv(DATA_PATH.joinpath("vet_review.csv"))
 print(df_review.to_dict(orient='records'))
 params = df_review.columns
+table_city_list = df_review["city"].unique()
 
 
 def generate_control_card():
@@ -116,6 +117,7 @@ def generate_control_card():
 app.layout = html.Div(
     id="app-container",
     children=[
+        html.Div(id='intermediate-value', style={'display': 'none'}),
         # Banner
         html.Div(
             id="banner",
@@ -141,7 +143,11 @@ app.layout = html.Div(
             className="eight columns pkcalc-data-table",
             children=[
                 # Patient Volume Heatmap
+                html.Br(),
+                html.P("Veterinary reviews"),
+
                 html.Div([
+
                     dash_table.DataTable(
                         id='review_table',
                         style_cell={
@@ -149,65 +155,160 @@ app.layout = html.Div(
                             'height': 'auto',  # wrapped text in column
                             'textAlign': 'left',
                             'overflowY': 'auto',
-                            'overflowX': 'auto',
+                            # 'overflowX': 'auto',
                             'fontSize': 12,
                             'font-family': 'sans-serif',
+                            'minWidth': '20px',
                         },
                         style_data={
-                            'maxHeight': '30px',
+                            'maxHeight': '50px',
                             # 'width': '150px', 'minWidth': '150px', 'maxWidth': '200px',
                             # 'textOverflow': 'ellipsis',
+                        },
+                        style_table={
+                            'maxHeight': '80ex',
+                            'width': '100%',
+                            'maxWidth': '100%',
                         },
                         # style header
                         style_header={
                             'fontWeight': 'bold',
-                            'fontSize': 15,
+                            'fontSize': 13,
                             'color': 'white',
                             'backgroundColor': '#357EC7',
+                            'textAlign': 'center',
                         },
                         style_data_conditional=[
+                            {
+                                'if': {'column_id': 'comment'},
+                                'width': '260px',
+                            },
+                            {
+                                'if': {'column_id': 'hospital_name'},
+                                'width': '10px',
+                            },
+                            {
+                                'if': {'column_id': 'doctor_name'},
+                                'width': '10px',
+                            },
+                            {
+                                'if': {'column_id': 'city'},
+                                'width': '15px',
+                            },
+                            {
+                                'if': {'column_id': 'breed'},
+                                'width': '10px',
+                            },
+                            {
+                                'if': {'column_id': 'rating'},
+                                'width': '10px',
+                            },
+                            {
+                                'if': {'column_id': 'disease'},
+                                'width': '20px',
+                            },
                             {
                                 # stripped rows
                                 'if': {'row_index': 'odd'},
                                 'backgroundColor': 'rgb(248, 248, 248)'
                             },
                             {
-                                # highlight one row
-                                'if': {'row_index': 4},
-                                "backgroundColor": "#3D9970",
-                                'color': 'white'
-                            }
+                                'if': {
+                                    # comparing columns to each other
+                                    'filter_query': '{rating} = 5',
+                                    'column_id': 'hospital_name'
+                                },
+                                'backgroundColor': '#3EA055',
+                                'color': 'white',
+                            },
+                            {
+                                'if': {
+                                    # comparing columns to each other
+                                    'filter_query': '{rating} = 4',
+                                    'column_id': 'hospital_name'
+                                },
+                                'backgroundColor': '#F3E5AB',
+                            },
+                            {
+                                'if': {
+                                    # comparing columns to each other
+                                    'filter_query': '{rating} = 2',
+                                    'column_id': 'hospital_name'
+                                },
+                                'backgroundColor': '#F5F5DC',
+                            },
+                            {
+                                'if': {
+                                    # comparing columns to each other
+                                    'filter_query': '{rating} = 3',
+                                    'column_id': 'hospital_name'
+                                },
+                                'backgroundColor': '#736F6E',
+                                'color': 'white',
+                            },
+                            {
+                                'if': {
+                                    # comparing columns to each other
+                                    'filter_query': '{rating} = 1',
+                                    'column_id': 'hospital_name'
+                                },
+                                'backgroundColor': '#2C3539',
+                                'color': 'white',
+                            },
                         ],
                         columns=(
                             [{'id': p, 'name': p} for p in params]
                         ),
                         data=df_review.to_dict(orient='records'),
                         editable=True,
-                        # filter_action='native',
+                        filter_action='native',
+                        sort_action='native',
+                        sort_mode='multi',
+                        # page_action='native',
+                        # page_size=10,
+                        virtualization=True,
+                        page_action='none',
+                        # row_selectable="multi",
+                        row_deletable=True,
                     ),
                     # dcc.Graph(id='table-editing-simple-output')
+
                 ]
                 ),
             ],
         ),
     ],
+
 )
 
 
 @app.callback(
-    Output("wait_time_table", "children"),
+    Output('intermediate-value', 'children'),
     [
-        Input("date-picker-select", "start_date"),
-        Input("date-picker-select", "end_date"),
-        Input("clinic-select", "value"),
-        Input("admit-select", "value"),
-        Input("patient_volume_hm", "clickData"),
-        Input("reset-btn", "n_clicks"),
+        Input("hospital-name", "value"),
+        Input("doctor-name", "value"),
+        Input("city-select", "value"),
+        Input("breed-select", "value"),
+        Input("disease-name", "value"),
+        Input("rating-select", "value"),
+        Input("detail-comments", "value"),
     ]
 )
-def update_table(start, end, clinic, admit_type, heatmap_click, reset_click, *args):
+def update_table(hospital, doctor, city, breed,
+                 disease, rating, comment):
 
-    return
+    dict_new_review = {}
+    dict_new_review['hospital_name'] = hospital
+    dict_new_review['doctor_name'] = doctor
+    dict_new_review['city'] = city
+    dict_new_review['breed'] = breed
+    dict_new_review['disease'] = disease
+    dict_new_review['rating'] = rating
+    dict_new_review['comment'] = comment
+
+    #df_review = df_review.append(dict_new_review, ignore_index=True)
+
+    #return df_review.to_dict(orient='records')
 
 
 # Run the server
